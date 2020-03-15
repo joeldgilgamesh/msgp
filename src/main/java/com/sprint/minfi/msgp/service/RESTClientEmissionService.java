3da026d4979cee3bf899c49c3aa4db4647982c4f
yannick.mengue <yannick.mengue>
2020-03-15 10:31:42 +0100

    add restclient to create emission after reconcilied process

--- src/main/java/com/sprint/minfi/msgp/service/RESTClientEmissionService.java
+++ src/main/java/com/sprint/minfi/msgp/service/RESTClientEmissionService.java
@@ -1,14 +1,22 @@
 package com.sprint.minfi.msgp.service;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.sprint.minfi.msgp.client.AuthorizedFeignClient;
+import com.sprint.minfi.msgp.domain.enumeration.Statut;
 import com.sprint.minfi.msgp.service.dto.HistoriquePaymentDTO;
 
 @AuthorizedFeignClient(name = "spminfimsged")
 public interface RESTClientEmissionService {
 
+	//cette demande doit etre envoyé sans Id
 	@PostMapping("/api/historiserEmissions")
-	public ResponseEntity<HistoriquePaymentDTO> historiserEmission(Long emissionId);
+	public ResponseEntity<HistoriquePaymentDTO> historiserEmission(@RequestBody HistoriquePaymentDTO historiquePaymentDTO);
+	
+	@PutMapping("/api/updateEmission/{refEmi}/{status}")
+	public ResponseEntity<String> updateEmission(@PathVariable String refEmis, @PathVariable Statut status);
 }
