f1d5b1be5f2376eedd9334e04742e9b2062a9c15
yannick.mengue <yannick.mengue>
2020-04-24 12:14:28 +0200

    console log code v2

--- src/main/java/com/sprint/minfi/msgp/web/rest/PaymentResource.java
+++ src/main/java/com/sprint/minfi/msgp/web/rest/PaymentResource.java
@@ -201,10 +201,10 @@ public class PaymentResource {
     													@PathVariable String status_code) {//cette methode sera démarrer par un client feign configuré dans mstransaction
 
     	String resultat = "Success";
-    	Statut status = Statut.VALIDATED;
+    	Statut status = null;
 		Payment payment = new Payment();
 		System.out.println("--------------------------- status code --> " + status_code);
-//		if (status_code == "100") status = Statut.VALIDATED;
+		if (status_code == "100") status = Statut.VALIDATED;
 		if (status_code == "400") status = Statut.CANCEL;
 		
 		//create transaction
