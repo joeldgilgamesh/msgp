2a124ca66dbccfa614e3ef7be81f131a7d35ff85
Herbin Tsobeng <herbin@modernappfactory.com>
2020-05-04 16:35:56 +0100

    refactor EventListener and activate audit datas on json

--- src/main/java/com/sprintpay/minfi/msgp/domain/EvenementListener.java
+++ src/main/java/com/sprintpay/minfi/msgp/domain/EvenementListener.java
@@ -1,6 +1,5 @@
 package com.sprintpay.minfi.msgp.domain;
 
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.slf4j.Logger;
@@ -8,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -24,40 +22,30 @@ public class EvenementListener<T> {
 
     @PostPersist
     public void entityPostPersist(T entity) {
-        Evenement<T> evenement=new Evenement<T>();
-        evenement.setValeur(entity);
-        evenement.setDateEvt(LocalDateTime.now());
-        evenement.setTypeEvenement("CREATE");
-        evenement.setTable_name(entity.getClass().getSimpleName());
-        kafkaTemplate.send(topic,applicationName+evenement.getDateEvt(),evenement);
-        log.debug("Evenement créé et transmi au brooker"+evenement);
-        System.out.println("Listening entity Post Persist : " + evenement);
+        prepareAndSendEvent("CREATE", entity);
     }
 
-   /* @PostUpdate
+    @PostUpdate
     public void entityPostUpdate(T entity) {
-        Evenement<T> evenement=new Evenement<T>();
-        evenement.setValeur(entity);
-        evenement.setDateEvt(LocalDateTime.now());
-        evenement.setTypeEvenement("UPDATE");
-        evenement.setTable_name(entity.getClass().getSimpleName());
-        kafkaTemplate.send(topic,applicationName+evenement.getDateEvt(),evenement);
-        log.debug("Evenement créé et transmi au brooker"+evenement);
-        System.out.println("Listening entity Post Update : " + evenement);
-    }*/
+        prepareAndSendEvent("UPDATE", entity);
+    }
 
-   /* @PostRemove
+    @PostRemove
     public void entityPostRemove(T entity) {
+        prepareAndSendEvent("DELETE", entity);
+    }
+
+    public void prepareAndSendEvent(String eventType, T entity){
         log.debug(" *************entité à envoyer au brooker*********"+entity);
         Evenement<T> evenement=new Evenement<T>();
         evenement.setValeur(entity);
         evenement.setDateEvt(LocalDateTime.now());
-        evenement.setTypeEvenement("DELETE");
+        evenement.setTypeEvenement(eventType);
         evenement.setTable_name(entity.getClass().getSimpleName());
         kafkaTemplate.send(topic,applicationName+evenement.getDateEvt(),evenement);
         log.debug("Evenement créé et transmi au brooker"+evenement);
         System.out.println("Listening entity Post Remove : " + evenement);
-    }*/
+    }
 
     String eventSerializer(Object event){
         JsonMapper jsonMapper=new JsonMapper();
