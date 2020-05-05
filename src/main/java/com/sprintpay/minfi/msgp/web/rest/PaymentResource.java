99081e538fa5fa22733e1abbda2e8648816ef99e
yannick.mengue <yannick.mengue>
2020-05-05 11:58:05 +0200

    move id in justificatif v2

--- src/main/java/com/sprintpay/minfi/msgp/web/rest/PaymentResource.java
+++ src/main/java/com/sprintpay/minfi/msgp/web/rest/PaymentResource.java
@@ -306,6 +306,7 @@ public class PaymentResource {
 	    	justificatifPaiementDTO.setCode(payment.getCode());
 	    	justificatifPaiementDTO.setImputations(listImput);
 	    	justificatifPaiementDTO.setIdOrganisation(1L);
+	    	justificatifPaiementDTO.setTypeJustificatifPaiement("default");
 
 	    	restClientQuittanceService.genererRecuOuQuittance(justificatifPaiementDTO);
 		}
