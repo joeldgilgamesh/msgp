e570097165289a2126f30c5f3d74d471e1d255cb
yannick.mengue <yannick.mengue>
2020-06-22 11:22:15 +0200

    update payment status cancel to canceled

--- src/main/java/com/sprintpay/minfi/msgp/domain/enumeration/Statut.java
+++ src/main/java/com/sprintpay/minfi/msgp/domain/enumeration/Statut.java
@@ -4,5 +4,5 @@ package com.sprintpay.minfi.msgp.domain.enumeration;
  * The Statut enumeration.
  */
 public enum Statut {
-    DRAFT, PENDING, VALIDATED, RECONCILED, CANCEL
+    DRAFT, PENDING, VALIDATED, RECONCILED, CANCELED
 }
