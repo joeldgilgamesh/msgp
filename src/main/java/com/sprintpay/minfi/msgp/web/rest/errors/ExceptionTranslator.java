7ed56359585fd694a417ce8d1082613cc9067e40
yannick.mengue <yannick.mengue>
2020-09-01 12:14:30 +0200

    add object request to handle badRequest v4

--- src/main/java/com/sprintpay/minfi/msgp/web/rest/errors/ExceptionTranslator.java
+++ src/main/java/com/sprintpay/minfi/msgp/web/rest/errors/ExceptionTranslator.java
@@ -90,7 +90,7 @@ public class ExceptionTranslator implements ProblemHandling, SecurityAdviceTrait
                 .withStatus(defaultConstraintViolationStatus())
                 .with(MESSAGE_KEY, ErrorConstants.DEFAULT_TYPE)
                 .with(FIELD_ERRORS_KEY, details)
-                .with("request", request.getParameter("DetailVersementIntermediaireDTO"))
+                .with("request", request.getNativeRequest())
                 .build();
 //        return create(ex, request, HeaderUtil.createFailureAlert(applicationName, true, ex.getEntityName(), ex.getErrorKey(), ex.getMessage()));
     	return new ResponseEntity<>(problem, HttpStatus.BAD_REQUEST);
