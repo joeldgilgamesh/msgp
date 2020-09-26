9dabe1d18d5d171f1e79b8ab68dd068fc6d0512e
yannick.mengue <yannick.mengue>
2020-09-26 15:19:35 +0200

    update versement summ

--- src/main/java/com/sprintpay/minfi/msgp/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/sprintpay/minfi/msgp/service/impl/PaymentServiceImpl.java
@@ -1,18 +1,13 @@
 package com.sprintpay.minfi.msgp.service.impl;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,7 +20,6 @@ import com.sprintpay.minfi.msgp.service.PaymentService;
 import com.sprintpay.minfi.msgp.service.dto.PaymentDTO;
 import com.sprintpay.minfi.msgp.service.dto.TransactionDTO;
 import com.sprintpay.minfi.msgp.service.mapper.PaymentMapper;
-import com.sprintpay.minfi.msgp.utils.ResponseSumm;
 
 /**
  * Service Implementation for managing {@link Payment}.
@@ -223,26 +217,26 @@ public class PaymentServiceImpl implements PaymentService {
 		return resp;
 	}
 
-	@Scheduled(fixedDelay = 60000)
-	public void test() {
-		
-		List<MeansOfPayment> AllMeans = new ArrayList<>();
-		List<JSONObject> listePaymentSummByMeansOfPayment = new ArrayList<>();
-		Map<String, Object> element = new HashMap<>();
-		
-		for (MeansOfPayment meansOfPayment : MeansOfPayment.values()) {
-			AllMeans.add(meansOfPayment);
-		}
-		
-		AllMeans.stream().forEach(meansOfPaymemnt -> 
-		{
-			element.put("MeansOfPayment", meansOfPaymemnt);
-			Double amount = paymentRepository.summReversementByMeansOfPayment(meansOfPaymemnt);
-			Double amountSend = amount != null ? amount : 0d;
-			element.put("Amount", amountSend);
-			listePaymentSummByMeansOfPayment.add(new JSONObject(element));
-		});
-		 System.out.println(listePaymentSummByMeansOfPayment);
-	}
+//	@Scheduled(fixedDelay = 60000)
+//	public void test() {
+//		
+//		List<MeansOfPayment> AllMeans = new ArrayList<>();
+//		List<JSONObject> listePaymentSummByMeansOfPayment = new ArrayList<>();
+//		Map<String, Object> element = new HashMap<>();
+//		
+//		for (MeansOfPayment meansOfPayment : MeansOfPayment.values()) {
+//			AllMeans.add(meansOfPayment);
+//		}
+//		
+//		AllMeans.stream().forEach(meansOfPaymemnt -> 
+//		{
+//			element.put("MeansOfPayment", meansOfPaymemnt);
+//			Double amount = paymentRepository.summReversementByMeansOfPayment(meansOfPaymemnt);
+//			Double amountSend = amount != null ? amount : 0d;
+//			element.put("Amount", amountSend);
+//			listePaymentSummByMeansOfPayment.add(new JSONObject(element));
+//		});
+//		 System.out.println(listePaymentSummByMeansOfPayment);
+//	}
 
 }
